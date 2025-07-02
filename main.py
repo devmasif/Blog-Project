d622cb5d8427c7a96553393168c073f6cc95d248
devmasif <m.ali83075@gmail.com>
2025-07-02 16:03:21 +0500

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from routes.auth_routes import router as auth_router
 from routes.post_routes import router as post_router
 from routes.comment_routes import router as comment_router
 from routes.like_routes import router as likes_router
-
+import uvicorn
 
 app = FastAPI()
 
@@ -16,3 +16,6 @@ app.include_router(auth_router)
 app.include_router(post_router)
 app.include_router(comment_router)
 app.include_router(likes_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
